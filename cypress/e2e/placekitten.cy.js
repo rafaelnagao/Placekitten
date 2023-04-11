7665130244a02ae7a5b97336f27218b625349549
R.Yo <rafaelnagao@msn.com>
2023-04-11 20:11:35 +0100

    https://github.com/rafaelnagao/Placekitten

---
+++ cypress/e2e/placekitten.cy.js
@@ -0,0 +1,51 @@
+describe('Placekitten', () => {
+  beforeEach(() => {
+    cy.visit('https://placekitten.com/')
+  })
+
+  context('Check page elements', () => {
+    it('Check title', () => {
+      cy.title()
+        .should('include', '{placekitten} - Placeholder kitten images for developers')
+    })
+
+    it('Check header', () => {
+      cy.get('h1')
+        .should('have.text', 'placekitten')
+    })
+
+    it('Check text', () => {
+      cy.get('[title="It fits a gap in the market. "]')
+        .find('strong')
+        .should('have.text', 'A quick and simple service for getting pictures of kittens for use as placeholders in your designs or code.')
+    })
+
+    it('Check links', () => {
+      cy.get('[href="/200/300"]')
+        .should('have.text', 'http://placekitten.com/200/300')
+      cy.get('[href="/g/200/300"]')
+        .should('have.text', 'http://placekitten.com/g/200/300')
+    })
+
+    it('Check images', () => {
+      for (let i = 0; i <= 7; i++) {
+        cy.get(`#image-${i}`).should('be.visible');
+      }
+    })
+
+    it.only('Check footer', () => {
+      cy.get('#footer > p')
+        .find('a')
+        .should(($links) => {
+          expect($links).to.have.length(3);
+
+          const hrefs = ['http://mark.james.name/', 'https://placekitten.com/attribution.html', 'http://placehold.it/'];
+
+          $links.each((index, link) => {
+            expect(link.href).to.equal(hrefs[index]);
+          });
+        });
+
+    })
+  })
+})
